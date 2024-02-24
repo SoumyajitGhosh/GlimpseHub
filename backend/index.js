e0264acbf40bec7f4d23972f648c77db944822c9
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-02-24 23:46:43 +0530

    BUILD: Backend Database connection

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const socketio = require('socket.io');
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+
+
+(async function () {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+        });
+        console.log("Connected to DataBase");
+    }
+    catch (err) {
+        throw new Error(err);
+    }
+})();
