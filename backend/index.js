634f5ab4f55829d8f75a666abea75574ccb6cdda
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-10-29 12:54:50 +0530

    Fix:Dependencies, initialize frontend

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,9 @@ if (process.env.NODE_ENV === 'production') {
 (async function () {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
         });
         console.log('Connected to database');
     } catch (err) {
