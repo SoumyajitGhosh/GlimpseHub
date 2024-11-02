ae52647b3524fdb9fd086f4962cf18a9731702e4
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-11-02 11:23:50 +0530

    Fix:Notifications Bug

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Card = ({ className, style, children }) => {
-    const cardClassNames = classNames({
-        card: true,
-        [className]: className,
-    });
+const Card = React.forwardRef(({ className, style, children }, ref) => {
+  const cardClassNames = classNames({
+    card: true,
+    [className]: className,
+  });
 
-    return (
-        <div className={cardClassNames} style={style}>
-            {children}
-        </div>
-    );
-};
+  return (
+    <div ref={ref} className={cardClassNames} style={style}>
+      {children}
+    </div>
+  );
+});
 
 export default Card;
